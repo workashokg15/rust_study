81d15de541ad3642fefcfb54ab3660a4ac8b7d17
Ashok Gopalakrishnan <workashok.g15@gmail.com>
2025-01-07 12:09:10 +0530

    options also

--- basics/src/main.rs
+++ basics/src/main.rs
@@ -1,7 +1,14 @@
+use std::result::Result;
+
 #[derive(Debug)]
 pub enum Res<T, E> {
     Thing(T),
     Error(E),
+}
+
+pub enum Option<T> {
+    Some(T),
+    None,
 }
 
 fn main() {
@@ -11,24 +18,24 @@ fn main() {
     let b = divide(5, 0);
 
     match a {
-        Res::Thing(v) => println!("a = {:?}", v),
+        Result::Ok(v) => println!("a = {}", v),
         _ => {}, 
     }
 
-    if let Res::Thing(v) = a {
+    if let Result::Ok(v) = a {
         println!("val is {}", v);
     }
 
     match b {
-        Res::Thing(v) => println!("b = {}", v),
-        Res::Error(s) => println!("Error : {}", s),
+        Result::Ok(v) => println!("b = {}", v),
+        Result::Err(s) => println!("Error : {}", s),
     }
 }
 
-fn divide(a: i32, b: i32) -> Res<i32, String> {
+fn divide(a: i32, b: i32) -> Result<i32, String> {
     if b == 0 {
-        return Res::Error("Cannot divide by 0".to_string());
+        return Result::Err("Cannot divide by 0".to_string());
     }
-    Res::Thing(a / b)
+    Result::Ok(a / b)
 }
 
